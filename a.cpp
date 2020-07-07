561d9ca6ea946d0fe335d1d73b7df432a01fed9a
Naman Goyal <60535864+Whistler27@users.noreply.github.com>
2020-07-07 16:43:03 +0530

    Longest Palindromic Substring

--- a.cpp
+++ a.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
-    
-    void sum(set<vector<int>> &s,vector<int> &a, vector<int> &cur,int target,int ind){
-     if(target<0) return;
-        if(target==0) s.insert(cur);
+    string longestPalindrome(string s) {
+        int n=s.size();
+        string ans;
+        int len;
         
-        if(ind==a.size()) return;
-        if(target-a[ind]>=0){
-            cur.push_back(a[ind]);
-            sum(s,a,cur,target-a[ind],ind+1);
-            cur.pop_back();
+        for(int mid=0;mid<n; mid++){
+            for(int x=0;mid-x>=0 && mid+x<n; x++){
+                if(s[mid-x]!=s[mid+x]) break;
+                int cur=2*x+1;
+                if(len<cur){
+                    len=cur;
+                    ans=s.substr(mid-x,len);
+                }
+            }
         }
-               
-        sum(s,a,cur,target,ind+1);
         
-    }
-    
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        for(int mid=0;mid<n;mid++){
+            for(int x=0;mid-x+1>=0 && mid+x<n;x++){
+                if(s[mid-x+1]!=s[mid+x]) break;
+                int cur=2*x;
+                if(len<cur){
+                    len=cur;
+                    ans=s.substr(mid-x+1,len);
+                }
+            }
+        }
         
-        int n=candidates.size();
-        sort(candidates.begin(),candidates.end());
-        set<vector<int>> s;
-        vector<int> cur;
-        sum(s,candidates,cur,target,0);
+      //  if(n==1) return s;
         
-        vector<vector<int>> ans;
-        for(auto i:s) ans.push_back(i);
         return ans;
         
     }
